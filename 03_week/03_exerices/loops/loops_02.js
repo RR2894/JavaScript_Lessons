41938102c39b7539e68f080af1b5854691e2c3d4
Randolph <randolph2894@gmail.com>
2024-01-26 18:46:55 +0200

    some exercies

--- 03_week/03_exerices/loops/loops_02.js
+++ 03_week/03_exerices/loops/loops_02.js
@@ -1,1 +1,11 @@
 // Develop a program that outputs all even, positive numbers less than 100 in the following pattern: 2, 98, 4, 96, 6, 94, and so on, up to the last number before 100. Display the result in a single line.
+
+let small = 2;
+let large = 98;
+let result = "";
+
+for (small; small <= 98; small += 2, large -= 2) {
+  result += `${small} ${large} `;
+}
+
+console.log(result);
--- 03_week/03_exerices/loops/loops_03.js
+++ 03_week/03_exerices/loops/loops_03.js
@@ -1,1 +1,14 @@
 // Create a program that continuously prompts the user to input distance (in kilometers) and time (in hours) and then calculates the average speed. The program should terminate when the user enters 0 for the distance. Upon receiving a distance of 0, the program should not prompt for any further input.
+
+let distance;
+let time;
+
+while (distance != 0) {
+  distance = Number(prompt("input distance"));
+
+  if (distance == 0) break;
+
+  time = Number(prompt("input number"));
+  let medianSpeed = distance / time;
+  console.log(medianSpeed);
+}
