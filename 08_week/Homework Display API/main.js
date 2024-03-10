bb857aa410d521f7565e268e100fdb78d8968757
Randolph <randolph2894@gmail.com>
2024-03-11 00:34:13 +0200

    update

--- 08_week/Homework Display API/main.js
+++ 08_week/Homework Display API/main.js
@@ -1,22 +1,29 @@
- function randomFacts() {
-    let xmlhttp = new XMLHttpRequest();
-    let url = "https://api.chucknorris.io/jokes/random";
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        let json = JSON.parse(this.responseText);
-        parseResponse(json);
-      }   
-    };
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
- }
+function randomFacts() {
+  let xmlhttp = new XMLHttpRequest();
+  let url = 'https://api.chucknorris.io/jokes/random';
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let json = JSON.parse(this.responseText);
+      parseResponse(json);
+    }
+  };
+  xmlhttp.open('GET', url, true);
+  xmlhttp.send();
+}
 
- randomFacts();
+function parseResponse(json) {
+  document.getElementById('data').innerHTML = '<b>' + json['value'] + '</b>';
+}
 
- function parseResponse(json) {
-    document.getElementById("data").innerHTML = "<b>" + json["value"] + "</b>";
- }
+document.getElementById('logo').addEventListener('click', function () {
+  randomFacts();
+});
 
- document.getElementById("logo").addEventListener("click", function() {
-    randomFacts(); 
- })
+const postContainer = document.getElementById('postContainer');
+const button = document.createElement('button');
+button.textContent = 'Get a FACT';
+button.addEventListener('click', () => {
+  randomFacts();
+});
+
+postContainer.appendChild(button);
